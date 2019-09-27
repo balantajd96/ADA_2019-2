16a0e08f85496813ff38bb55e0dbd24d7ed6b01f
Xavier Garzon Lopez <xg@PC40MAC.local>
2019-09-27 16:01:06 -0500

    garden terminado con break

--- HM4/10001-Garden of Eden/eden.py
+++ HM4/10001-Garden of Eden/eden.py
@@ -33,54 +33,59 @@ def main():
         num=list(map(str, stdin.readline().split()))
 main()
 """
+verify=[]
 def paths(string, goal, index, mask):
     tmp=""
     paths=[]; i=0
-    print("--------")
-    print(mask)
-    print(goal)
+    #print("--------")
+    #print(mask)
+    #print(goal)
     for i in range(len(mask)):
         if mask[i]==goal[index]:
             paths.append("{:03b}".format(i))
     return paths
 
 def is_correct(string, path, index):
-    #Revisar
+    """
+    path: 3-bits string
+    index: index to place path
+    """
     ans=True
     if index==0:
-        if string[-1]==2 and string[0]==2 and string[1]==2:
-            string[-1]=path[0]
-            string[0]=path[0] 
-            string[1]=path[0]
-    elif 1<=i<=lent-2:
-        if string[i-1]==2 and string[i]==2 and string[i+1]==2:
-            string[i-1]=path[0]
-            string[i]=path[0] 
-            string[i+1]=path[0]
+        string[-1]=path[0]
+        string[0]=path[1]
+        string[1]=path[2]
+    elif 1<=index<=len(string)-2:
+        if string[index-1]==path[0] and string[index]==path[1]:
+            string[index+1]=path[1]
+        else:
+            ans=False
     else:
-        if string[i-1]==2 and string[i]==2 and string[0]==2:
-            string[i-1]=path[0]
-            string[i]=path[0] 
-            string[0]=path[0]
-
+        if string[index-1]==path[0] and string[index]==path[1] and string[0]==path[2]:
+           pass
+        else:
+            ans=False 
+    return ans
 def solve(goal, string, len_string, mask, index):
+    global verify
     ans=None
     if index==len(string):
         ans=True
         for i in range(len(string)):
-            if  string[i]=="2":ans=False
+            if  string[i]=="2":
+                ans=False
     else:
-        print(goal, string, len_string, mask, index)
+        #print(goal, string, len_string, mask, index)
         _paths=paths(string, goal, index, mask)
-        for i in range(len(_paths)):
-            tmp=string
-            if is_correct(string, _paths[i], index):
-                solve(goal, string, len_string, mask, index+1)
-            else:
-                string=tmp
-
-
-
+        if _paths==[]:
+            return True
+        else:
+            for i in range(len(_paths)):
+                tmp=string
+                if is_correct(string, _paths[i], index):
+                    solve(goal, string, len_string, mask, index+1)
+                else:
+                    ans=False; break
     return ans
 
 def main():
@@ -90,6 +95,9 @@ def main():
     mask="{:08b}".format(204)
     for i in range(len_string): string+="2"
 
-    print(solve(goal, string, len_string, mask, 0))
+    tmp=solve(goal, list(string), len_string, mask, 0)
+    if tmp==True:print("GARDEN OF EDEN")
+    else:print("REACHABLE")
+
 
 main()
