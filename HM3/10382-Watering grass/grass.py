10dec0f89b001a3d0a8746385c18e563f9893865
Xavier Garzon Lopez <xg@PC38MAC.local>
2019-09-30 11:53:27 -0500

    Grass

--- HM3/10382-Watering grass/grass.py
+++ HM3/10382-Watering grass/grass.py
@@ -1,59 +1,52 @@
+import math
 from sys import stdin
-def sort_ran(arr):
-	"""
-	toma dos arreglos, los junta en tuplas
-	y los ordena de mayor a menor por posicion.
-	Después pasa a distancias por medio de 
-	tuplas7
-	"""
-	new_arr=[]
-	for i in range(len(arr)):
-		hi=arr[i][0]+arr[i][1]-1
-		lo=arr[i][0]-arr[i][1]+1
-		new_arr.append((lo,hi))
-	new_arr.sort(key=lambda item: (item[0],item[1]),reverse=False)
-	return new_arr
+def ranges(pos,w_med,r):
+    tmp=math.sqrt(r**2-(w_med)**2)
+    return (pos-tmp,pos+tmp)
 
-def solve3(N,l,w,arr):
-	m=max(l,w); cnt=1
-	i=0; ranges=sort_ran(arr)
+def mic_wf(L,H,a):
+  """
+  Codigo de ADA-2019-1 
+  Minimal Covering Algorithm with failure checking
+  """
+  a.sort()
+  ans,low,n,ok,N = list(),L,0,True,len(a)
+  cnt=0
+  while ok and low<H and n!=N:
+    ok = a[n][0]<=low
+    best,n = n,n+1
+    while ok and n!=N and a[n][0]<=low:
+      if a[n][1]>a[best][1]:
+        best = n
+      n += 1
+    ans.append(best)
+    cnt+=1
+    low = a[best][1]
+  ok = ok and low>=H
+  if ok==False:
+    ans = list()
+  if ans==[]:cnt=-1
+  return cnt
 
-	if ranges[0][0]>0:
-		cnt=-1
-	else:
-		for i in range(len(ranges)):
-			if ranges[i][0]<0 and ranges[i][1]>=m:
-				return 1
-		lo=ranges[0][0]
-		hi=ranges[0][1]
-		i=1
-		while i<N:
-			candidates=[]
-			limit=hi
-			while i<N and limit-ranges[i][0]>=-1:
-				candidates.append((ranges[i][0],ranges[i][1]))
-				i+=1
-			maxi=-9999; chosen=None
-			for j in range(len(candidates)):
-				diff=candidates[j][1]
-				if diff>maxi:
-					maxi=diff
-					chosen=j			
-			hi=candidates[chosen][1]
-			cnt+=1
-		if hi<=m:cnt=-1
-	return cnt
+def solve(N,l,w,arr):
+    candidates=[]
+    for i in range(N):
+        w_med=w/2
+        if arr[i][1]>=(w_med):
+            candidates.append(ranges(arr[i][0],w_med,arr[i][1]))
+    ans=mic_wf(0,l,candidates)
+    return ans
 
 def main():
-	tmp=True
-	while tmp:
-		try:
-			N,l,w=map(int, stdin.readline().split())
-			arr=[]
-			for i in range(N):
-				pos,r=map(int, stdin.readline().split())
-				arr.append((pos,r))
-			print(solve3(N,l,w,arr))
-		except:
-			tmp=False			
+    tmp=True
+    while tmp:
+        try:
+            N,l,w=map(int, stdin.readline().split())
+            arr=[]
+            for i in range(N):
+                pos,r=map(int, stdin.readline().split())
+                arr.append((pos,r))
+            print(solve(N,l,w,arr))
+        except:
+            tmp=False           
 main()
